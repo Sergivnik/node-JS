c0d102c2ff086433add61b4159c998a024001c60
Serg <sergivnik@mail.ru>
2021-01-12 20:01:21 +0300

    v-1.1

---
+++ hello-npm.js
@@ -0,0 +1,8 @@
+var colors = require("colors");
+console.log("Hello!!".green); 
+console.log("I like yellow".yellow+" and blue".blue);
+console.log("And I don't like red".red);
+
+const Audic = require("audic");
+// sorry for audio it was first i'd found)))
+new Audic("sound.mp3").play();
