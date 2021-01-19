17f45a7057ea1842b56d5b281eb871558eeba0f4
Serg <sergivnik@mail.ru>
2021-01-19 10:45:15 +0300

    v-2.5

--- lesson-2.js
+++ lesson-2.js
@@ -1,26 +1,24 @@
-// node lesson-2.js  result.json 
+// node lesson-2.js  result.json
 // команда запуска
 
 var argv = require("minimist")(process.argv.slice(2));
 var colors = require("colors");
 var readline = require("readline");
 var fs = require("fs");
-const { time } = require("console");
 
-//const util = require("util");
-console.dir(argv._[0]);
-//console.log(util.inspect(argv));
-//console.dir(argv);
 var rl = readline.createInterface({
   input: process.stdin, // ввод из стандартного потока
   output: process.stdout, // вывод в стандартный поток
 });
 let lastNumber;
-let report;
+let report=[];
 fs.readFile(argv._[0], function (err, data) {
-  if (err) throw err;
-  report = JSON.parse(data);
-  lastNumber = report[report.length - 1].game_number + 1;
+  if (err) {
+    console.log("error", err);
+  } else {
+    report = JSON.parse(data);
+    lastNumber = report[report.length - 1].game_number + 1;
+  }
 });
 console.log("Type ypur bet (1 or 2)");
 rl.on("line", function (cmd) {
@@ -38,43 +36,45 @@ rl.on("line", function (cmd) {
   report.push({ game_number: lastNumber, result: resultGame });
 
   fs.writeFile(argv._[0], JSON.stringify(report, null, "\t"), function (err) {
-    if (err) throw err;
-    //console.log(data.toString());
+    if (err) console.log("error", err);
   });
   rl.close();
 
   fs.readFile(argv._[0], function (err, data) {
-    if (err) throw err;
-    report = JSON.parse(data);
-    //console.log(report);
-    let totalWin = 0;
-    let totalLoss = 0;
-    let seriesWin = 1;
-    let seriesLoss = 1;
-    let maxSeriesWin = 1;
-    let maxSeriesLoss = 1;
-    let lastResult = "";
-    report.forEach((element) => {
-      if (element.result == "Win") totalWin++;
-      if (element.result == "Loss") totalLoss++;
-      if (element.result == lastResult && element.result == "Win") {
-        seriesWin++;
-        if (seriesWin > maxSeriesWin) maxSeriesWin = seriesWin;
-      }
-      if (element.result == lastResult && element.result == "Loss") {
-        seriesLoss++;
-        if (seriesLoss > maxSeriesLoss) maxSeriesLoss = seriesLoss;
-      }
-      if (element.result != lastResult) {
-        seriesWin = 1;
-        seriesLoss = 1;
-      }
-      lastResult = element.result;
-    });
-    console.log("Total games - ".yellow, report.length);
-    console.log("Total Wins - ".yellow, totalWin);
-    console.log("Total Loss - ".yellow, totalLoss);
-    console.log("Max wins series - ".yellow, maxSeriesWin);
-    console.log("Max losses series - ".yellow, maxSeriesLoss);
+    if (err) {
+      console.log("error", err);
+    } else {
+      report = JSON.parse(data);
+      //console.log(report);
+      let totalWin = 0;
+      let totalLoss = 0;
+      let seriesWin = 1;
+      let seriesLoss = 1;
+      let maxSeriesWin = 1;
+      let maxSeriesLoss = 1;
+      let lastResult = "";
+      report.forEach((element) => {
+        if (element.result == "Win") totalWin++;
+        if (element.result == "Loss") totalLoss++;
+        if (element.result == lastResult && element.result == "Win") {
+          seriesWin++;
+          if (seriesWin > maxSeriesWin) maxSeriesWin = seriesWin;
+        }
+        if (element.result == lastResult && element.result == "Loss") {
+          seriesLoss++;
+          if (seriesLoss > maxSeriesLoss) maxSeriesLoss = seriesLoss;
+        }
+        if (element.result != lastResult) {
+          seriesWin = 1;
+          seriesLoss = 1;
+        }
+        lastResult = element.result;
+      });
+      console.log("Total games - ".yellow, report.length);
+      console.log("Total Wins - ".yellow, totalWin);
+      console.log("Total Loss - ".yellow, totalLoss);
+      console.log("Max wins series - ".yellow, maxSeriesWin);
+      console.log("Max losses series - ".yellow, maxSeriesLoss);
+    }
   });
 });
