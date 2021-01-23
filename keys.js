ea5119c989602b987c33080cf167f236a364a2c5
Serg <sergivnik@mail.ru>
2021-01-23 23:29:32 +0300

    v-3.4

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  IAM_TOKEN:
+    "t1.9euelZqbx5SbmJjLlonImsmezZmNy-3rnpWamZTIi5XLk5mby5STkc3IyZPl8_dAO1B_-e9nBAAw_t3z9wBqTX_572cEADD-.NuIh4fz95hYV3fcsUNx9z7TzcZUG-TuHVc6rX-O2EZxqPVnyfvKhpSEWkTHk-zksHBFL0GdgBwPWwN4QzJsRAA",
+  FOLDER: "b1ghe29nv5heb4q8ffol",
+};
--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const axios = require("axios");
 const express = require("express");
+const { IAM_TOKEN, FOLDER } = require("./keys");
 const app = express();
 app.use(express.static("."));
 app.use(express.json());
-const IAM_TOKEN =
-  "t1.9euelZqbx5SbmJjLlonImsmezZmNy-3rnpWamZTIi5XLk5mby5STkc3IyZPl8_dAO1B_-e9nBAAw_t3z9wBqTX_572cEADD-.NuIh4fz95hYV3fcsUNx9z7TzcZUG-TuHVc6rX-O2EZxqPVnyfvKhpSEWkTHk-zksHBFL0GdgBwPWwN4QzJsRAA";
-const FOLDER = "b1ghe29nv5heb4q8ffol";
 
 app.post("/translate", (req, response) => {
-  const newGood = req.body;
-  let result = axios
+  axios
     .post(
       "https://translate.api.cloud.yandex.net/translate/v2/translate/",
       {
         folder_id: FOLDER,
-        texts: [newGood.text],
+        texts: [req.body.text],
         targetLanguageCode: "ru",
       },
       {
@@ -25,13 +22,11 @@ app.post("/translate", (req, response) => {
       }
     )
     .then((res) => {
-      console.log(res.data.translations[0].text);
       response.send(res.data.translations[0].text);
     })
     .catch((err) => {
-      console.log(err);
+      response.send("err");
     });
-  //response.send(result.res);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
