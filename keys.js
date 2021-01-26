89aab898deeb7f311a8debaa79ccd3e3ad92afc3
Serg <sergivnik@mail.ru>
2021-01-26 21:48:47 +0300

    v-4.3

--- keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  IAM_TOKEN:
-    "t1.9euelZrIy4vImY-RnI2Lyc6PmJOVzO3rnpWamZTIi5XLk5mby5STkc3IyZPl8_dcOz9_-e83ZTMW_t3z9xxqPH_57zdlMxb-.zhrCkKxyIzti2OBYEm4ojdK9U1eg83zTgne-KwVkPyO4crbUHYNudXKdRT7-4-lAZPXcfabJECGaSGd-N_i2Bg",
-  FOLDER: "b1ghe29nv5heb4q8ffol",
-};
--- server.js
+++ server.js
@@ -1,37 +1,59 @@
-const axios = require("axios");
+
 const express = require("express");
 const path = require("path");
-const { IAM_TOKEN, FOLDER } = require("./keys");
 const app = express();
 var cookieParser = require("cookie-parser");
+
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.post("/translate", (req, response) => {
+app.post("/", (req, response) => {
   console.log("Cookies: ", req.cookies);
-  axios
-    .post(
-      "https://translate.api.cloud.yandex.net/translate/v2/translate/",
-      {
-        folder_id: FOLDER,
-        texts: [req.body.text],
-        targetLanguageCode: "ru",
-      },
-      {
-        headers: {
-          "Content-Type": "aaplication/json",
-          Authorization: "Bearer " + IAM_TOKEN,
-        },
-      }
-    )
-    .then((res) => {
-      response.send(res.data.translations[0].text);
-    })
-    .catch((err) => {
-      response.send("err");
-    });
+  const data = req.body;
+  console.log(data);
+
+  var request = require("request");
+  var cheerio = require("cheerio");
+  request("https://www.euro-football.ru/", function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      $(".main-news__item").each(function (i, element) {
+        var cols = $(this).text();
+        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
+        console.log(cols);
+        if (i > 5) return false;
+      });
+      console.log(" ");
+    }
+  });
+  request("https://www.f1news.ru/", function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      $(".b-news-list__title").each(function (i, element) {
+        var cols = $(this).text();
+        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
+        console.log(cols);
+        if (i > 5) return false;
+      });
+      console.log(" ");
+    }
+  });
+  request("https://allhockey.ru/", function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var f = $(".summary > a");
+      f.each(function (i, element) {
+        var cols = $(this).text();
+        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
+        console.log(cols);
+        if (i > 5) return false;
+      });
+    }
+  });
+  response.send("ffff")
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
