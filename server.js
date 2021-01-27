431ac6fddd3badd5df9ec9ae840cafc83672baed
Serg <sergivnik@mail.ru>
2021-01-27 21:20:49 +0300

    v-4.5

--- server.js
+++ server.js
@@ -10,44 +10,65 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-function getData(req) {
-  console.log(req);
-  axios
+async function getData(req) {
+  let listNews = [];
+  a = axios
     .get(req.site)
     .then((response) => {
       var $ = cheerio.load(response.data);
       $(req.selector).each(function (i, element) {
         var cols = $(this).text();
         cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
-        console.log(cols);
-        if (i > 3) return false;
+        listNews.push(cols);
+        if (i > req.number - 2) return false;
       });
-      console.log(" ");
+      listNews.push("");
+      return listNews;
     })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
+    .catch((err) => {
+      console.log("error", err);
     });
+  let result = await a;
+  return result;
 }
 
 app.post("/", (req, response) => {
+  let arrNews = [];
   console.log("Cookies: ", req.cookies);
   const data = req.body;
   console.log(data);
-  getData({
-    site: "https://www.euro-football.ru/",
-    selector: ".main-news__item",
-  });
-  getData({
-    site: "https://www.f1news.ru/",
-    selector: ".b-news-list__title",
-  });
-  getData({
-    site: "https://allhockey.ru/",
-    selector: ".summary > a",
-  });
-
-  response.send("ffff");
+  if (data.Football === "on") {
+    getData({
+      site: "https://www.euro-football.ru/",
+      selector: ".main-news__item",
+      number: data.numberNews,
+    })
+      .then((result) => {
+        arrNews.push("Футбол");
+        arrNews = arrNews.concat(result);
+      })
+  }
+  if (data.F1 === "on")
+    getData({
+      site: "https://www.f1news.ru/",
+      selector: ".b-news-list__title",
+      number: data.numberNews,
+    })
+      .then((result) => {
+        arrNews.push("Ф1");
+        arrNews = arrNews.concat(result);
+      })
+  if (data.hockey === "on")
+    getData({
+      site: "https://allhockey.ru/",
+      selector: ".summary > a",
+      number: data.numberNews,
+    })
+      .then((result) => {
+        arrNews.push("Хоккей");
+        arrNews = arrNews.concat(result);
+      })
+  setTimeout(()=>response.send(arrNews), 5000); 
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
