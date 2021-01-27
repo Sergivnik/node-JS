d19b8e065927a3a032adc423903a005bd6f28d15
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-01-27 15:20:27 +0300

    v-4.4

--- server.js
+++ server.js
@@ -1,59 +1,53 @@
-
+const axios = require("axios");
 const express = require("express");
 const path = require("path");
 const app = express();
 var cookieParser = require("cookie-parser");
+var cheerio = require("cheerio");
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+function getData(req) {
+  console.log(req);
+  axios
+    .get(req.site)
+    .then((response) => {
+      var $ = cheerio.load(response.data);
+      $(req.selector).each(function (i, element) {
+        var cols = $(this).text();
+        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
+        console.log(cols);
+        if (i > 3) return false;
+      });
+      console.log(" ");
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+}
 
 app.post("/", (req, response) => {
   console.log("Cookies: ", req.cookies);
   const data = req.body;
   console.log(data);
+  getData({
+    site: "https://www.euro-football.ru/",
+    selector: ".main-news__item",
+  });
+  getData({
+    site: "https://www.f1news.ru/",
+    selector: ".b-news-list__title",
+  });
+  getData({
+    site: "https://allhockey.ru/",
+    selector: ".summary > a",
+  });
 
-  var request = require("request");
-  var cheerio = require("cheerio");
-  request("https://www.euro-football.ru/", function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      $(".main-news__item").each(function (i, element) {
-        var cols = $(this).text();
-        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
-        console.log(cols);
-        if (i > 5) return false;
-      });
-      console.log(" ");
-    }
-  });
-  request("https://www.f1news.ru/", function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      $(".b-news-list__title").each(function (i, element) {
-        var cols = $(this).text();
-        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
-        console.log(cols);
-        if (i > 5) return false;
-      });
-      console.log(" ");
-    }
-  });
-  request("https://allhockey.ru/", function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      var f = $(".summary > a");
-      f.each(function (i, element) {
-        var cols = $(this).text();
-        cols = cols.replace(/^\s*/gm, "").replace(/\s+$/, "");
-        console.log(cols);
-        if (i > 5) return false;
-      });
-    }
-  });
-  response.send("ffff")
+  response.send("ffff");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
