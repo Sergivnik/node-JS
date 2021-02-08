a949f7acfb97920f2ab03eb6f9deb35446c113b8
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2021-02-08 14:52:54 +0300

    v-7.4

--- controlers/API.js
+++ controlers/API.js
@@ -1,4 +1,4 @@
-const db = require("./models/db.js");
+const db = require("../models/db.js");
 
 module.exports.taskGet = (req, res) => {
   db.query("SELECT * FROM listtodo").then(([data, fields]) => {
--- routers/APIRouter.js
+++ routers/APIRouter.js
@@ -3,3 +3,5 @@ const router = express.Router();
 const API = require("../controlers/API.js");
 
 router.get("/getDATA", API.taskGet);
+
+module.exports = router;
