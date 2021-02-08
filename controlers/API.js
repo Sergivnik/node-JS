0fca80bdb321f9eee3973684e1612f80c6f7990b
Serg <sergivnik@mail.ru>
2021-02-08 18:59:03 +0300

    v-7.6

--- controlers/API.js
+++ controlers/API.js
@@ -5,3 +5,20 @@ module.exports.taskGet = (req, res) => {
     res.send(data);
   });
 };
+module.exports.taskAdd = (req, res) => {
+  db.query("INSERT INTO listtodo SET ?", req.body).then(([data, fields]) => {
+    res.send(data);
+  });
+};
+module.exports.deleteID = (req, res) => {
+  db.query("DELETE FROM listtodo WHERE ?", req.body).then(([data, fields]) => {
+    res.send(data);
+  });
+};
+module.exports.completeID = (req, res) => {
+  db.query(`UPDATE listtodo SET ? where id=${req.body.id}`, [{ complete: "1" }]).then(
+    ([data, fields]) => {
+      res.send(data);
+    }
+  );
+};
--- routers/APIRouter.js
+++ routers/APIRouter.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const API = require("../controlers/API.js");
 
 router.get("/getDATA", API.taskGet);
+router.post("/addDATA", API.taskAdd);
+router.delete("/deleteID", API.deleteID);
+router.put("/completeID", API.completeID);
 
 module.exports = router;
